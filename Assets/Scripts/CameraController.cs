470d36450f14d28fe770825e9a47e26a4c27a08c
AlexMarrinan <alex@balroe.com>
2022-07-05 02:20:09 -0400

    First commit!!!

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,86 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum CameracDirection{
+    Behind = 0,
+    Left = 1,
+    Infront = 2,
+    Right = 3
+}
+
+[RequireComponent(typeof(Camera))]
+public class CameraController : MonoBehaviour
+{
+
+    public static CameraController instance;
+    void Awake(){
+        if (instance == null){
+            instance = this;
+            DontDestroyOnLoad(instance);
+        }
+        else{
+            Destroy(gameObject);
+        }
+    }
+    const int CAMERADIRECTION_MAX = 3;
+    const int CAMERA_DISTANCE = 30;
+    public Camera c;
+    public CameracDirection cd;
+    public Transform player;
+    public Vector3 cameraOffset;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        c = GetComponent<Camera>();
+        cd = CameracDirection.Infront;
+        cameraOffset = new Vector3(0, CAMERA_DISTANCE, -CAMERA_DISTANCE);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.anyKey){
+
+            //Rotate camera left
+            if (Input.GetKeyDown(KeyCode.Q)){
+                c.transform.Rotate(0f, 90.0f, 0.0f, Space.World);
+                player.transform.Rotate(0f, 90.0f, 0.0f, Space.World);
+                PlayerController.instance.RotateLeft();
+                //PlayerController.instance.startRotation = player.transform.rotation;
+                cd++;
+            }
+            //Rotate Camera right
+            if (Input.GetKeyDown(KeyCode.E)){
+                c.transform.Rotate(0.0f, -90.0f, 0.0f, Space.World);
+                player.transform.Rotate(0.0f, -90.0f, 0.0f, Space.World);
+                PlayerController.instance.RotateRight();
+                //PlayerController.instance.startRotation = player.transform.rotation;
+                cd--;
+            }
+            if ((int) cd > CAMERADIRECTION_MAX){
+                cd = 0;
+            }else if (cd < 0){
+                cd = (CameracDirection)CAMERADIRECTION_MAX;
+            }
+            switch (cd){
+                case CameracDirection.Behind: 
+                    cameraOffset = new Vector3(0, CAMERA_DISTANCE, CAMERA_DISTANCE);
+                    break;
+                case CameracDirection.Left: 
+                    cameraOffset = new Vector3(CAMERA_DISTANCE, CAMERA_DISTANCE, 0);
+                    break;
+                case CameracDirection.Right: 
+                    cameraOffset = new Vector3(-CAMERA_DISTANCE, CAMERA_DISTANCE, 0);
+                    break;
+                case CameracDirection.Infront: 
+                    cameraOffset = new Vector3(0, CAMERA_DISTANCE, -CAMERA_DISTANCE);
+                    break;
+            }
+        }
+
+        transform.position = player.transform.position + cameraOffset;
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,139 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public static PlayerController instance;
+    public SpriteRenderer sprite;
+    private Rigidbody rb;
+    private Animator animator;
+    private float walkSpeed = 12;
+    private float sprintSpeed = 24;
+    private bool sprinting = false;
+    void Awake(){
+        if (instance == null){
+            instance = this;
+            DontDestroyOnLoad(instance);
+        }
+        else{
+            Destroy(gameObject);
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        sprite = GetComponent<SpriteRenderer>();
+        rb = GetComponent<Rigidbody>();
+        animator = GetComponentInChildren<Animator>();
+        //startRotation = sprite.transform.rotation;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetPlayerInput();
+        //sprite.transform.rotation = startRotation;
+    }
+    private void OnCollisionEnter(Collision other) {
+        Debug.Log(other.collider.name);
+    }
+    public void GetPlayerInput(){
+        
+        if (Input.GetKeyDown(KeyCode.LeftShift)){
+            sprinting = true;
+            Debug.Log("Started sprinting");
+        }
+        if (Input.GetKeyUp(KeyCode.LeftShift)){
+            sprinting = false;
+            Debug.Log("Stopped sprinting");
+        }
+        if (Input.GetKeyUp(KeyCode.Space)){
+            Debug.Log("Attacking");
+        }
+        UpdateAnimationsAndMove();
+    }
+
+    public void UpdateAnimationsAndMove(){
+        Vector2 input = Vector2.zero; 
+        input.x = Input.GetAxis("Horizontal");
+        input.y = Input.GetAxis("Vertical");
+        input = Vector2.ClampMagnitude(input, 1);
+        
+        if (input != Vector2.zero){
+            
+            Vector3 forward = CameraController.instance.transform.forward;
+            Vector3 right = CameraController.instance.transform.right;
+            
+            forward.y = 0;
+            right.y = 0;
+
+            forward.Normalize();
+            forward.Normalize();
+
+            MovePlayer((forward*input.y + right*input.x)*Time.deltaTime*(sprinting ? sprintSpeed : walkSpeed));
+
+            animator.SetFloat("MoveX", input.x);
+            animator.SetFloat("MoveY", input.y);
+            animator.SetBool("Walking", true);
+        }else{
+            animator.SetBool("Walking", false);
+        }
+    }
+    public void MovePlayer(Vector3 direction){
+        rb.MovePosition(rb.position + direction);
+    }
+    private Vector3 PlayerCameraOffset(){
+        rb.velocity = new Vector3(0, 0, 0);
+        return CameraController.instance.cameraOffset;
+    }
+
+    public void RotateRight(){
+        float x = animator.GetFloat("MoveX");
+        float y = animator.GetFloat("MoveY");
+
+        if (x > 0){
+            animator.SetFloat("MoveY", -1);
+            animator.SetFloat("MoveX", 0);
+        }
+        else if (x < 0){
+            animator.SetFloat("MoveY", 1);
+            animator.SetFloat("MoveX", 0);
+        }
+        else if (y > 0){
+            animator.SetFloat("MoveY", 0);
+            animator.SetFloat("MoveX", 1);
+        }
+        else if (y < 0){
+            animator.SetFloat("MoveY", 0);
+            animator.SetFloat("MoveX", -1);
+        }else{//if not moved at all
+            animator.SetFloat("MoveY", 0);
+            animator.SetFloat("MoveX", -1);
+        }
+    }
+    public void RotateLeft(){
+        float x = animator.GetFloat("MoveX");
+        float y = animator.GetFloat("MoveY");
+
+        if (x < 0){
+            animator.SetFloat("MoveY", -1);
+            animator.SetFloat("MoveX", 0);
+        }
+        else if (x > 0){
+            animator.SetFloat("MoveY", 1);
+            animator.SetFloat("MoveX", 0);
+        }
+        else if (y < 0){
+            animator.SetFloat("MoveY", 0);
+            animator.SetFloat("MoveX", 1);
+        }
+        else if (y > 0){
+            animator.SetFloat("MoveY", 0);
+            animator.SetFloat("MoveX", -1);
+        }else{//if not moved at all
+            animator.SetFloat("MoveY", 0);
+            animator.SetFloat("MoveX", 1);
+        }
+    }
+}
